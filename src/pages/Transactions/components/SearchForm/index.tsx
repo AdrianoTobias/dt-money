a0c355b50a86fe65d0409d5e84893edf957ff186
Adriano <adrianoact@msn.com>
2024-08-21 23:54:58 -0300

    Performance - Applying memo

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -6,13 +6,39 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useContextSelector } from 'use-context-selector'
 import { TransactionsContext } from '../../../../contexts/TransactionsContext'
 
+import { memo } from 'react'
+/**
+ * Por que um componente renderiza?
+ *  - Hooks changed (estado, contexto, reducer)
+ *  - Props changed
+ *  - Parent rerendered
+ * 
+ * Qual o fluxo de renderização?
+ *  1. O React recria o HTML da interface do componente
+ *  2. Compara a versão do HTML recriado com a versão atual
+ *  3. SE mudou algo, então reescreve o HTML na tela
+ * 
+ * Memo:
+ *  0. Hooks changed, Props changed (deep comparison)
+ *  0.1 Compara a versão anterior dos hooks e das props
+ *  0.2 SE mudou algo, então permite a nova renderização
+ * 
+ * => Nem sempre o memo é mais rápido do que recriar o HTML.
+ * => O React já é rápido e renderizar componentes não é um problema,
+ * a menos que seja um componente muito grande (em termos de retorno),
+ * o qual pode causar lentidão e, neste caso, pode-se utilizar o memo.
+ * 
+ * ==> As alterações aqui feitas, são somente para demonstrar com o memo funciona.
+ */
+
+
 const searchFormSchema = z.object({
   query: z.string(),
 })
 
 type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
-export function SearchForm() {
+function SearchFormComponent() {
   const fetchTransactions = useContextSelector(
     TransactionsContext,
     context => {
@@ -45,3 +71,5 @@ export function SearchForm() {
     </SearchFormContainer>
   )
 }
+
+export const SearchForm = memo(SearchFormComponent)
